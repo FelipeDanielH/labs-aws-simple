0b0cd85f25020cb7f6f82b25df5f99f465584517
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2026-07-16 08:15:14 -0400

    Update Spline scene

--- apps/web/src/features/home/config/spline-scene.ts
+++ apps/web/src/features/home/config/spline-scene.ts
@@ -1,2 +1,2 @@
 export const HOME_SPLINE_SCENE_URL =
-  "https://my.spline.design/cutecomputerfollowcursor-t2nYvcrFzPfaJWM45QnLl5ka/scene.splinecode";
+  "https://my.spline.design/cutecomputerfollowcursor-Uao7mtPfaSm5p23mMGtiJybX/scene.splinecode";
